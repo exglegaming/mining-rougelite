7bbe61e97ab49426ae210a517da0d8819490d111
ExgleGaming <EaglesFan91x@outlook.com>
2026-01-29 21:28:57 -0500

    added ores

---
+++ scripts/resources/data/ores/ore_data.gd
@@ -0,0 +1,7 @@
+class_name OreData
+extends Resource
+
+
+@export var name: String
+@export var texture: Texture2D
+@export var value: int
--- scripts/resources/data/rocks/rock_data.gd
+++ scripts/resources/data/rocks/rock_data.gd
@@ -6,3 +6,4 @@ extends Resource
 @export var max_health: int
 @export var rarity: int
 @export var min_depth: int
+@export var ore_resource: OreData
