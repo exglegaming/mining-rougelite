682e6168c3617028881fe7ce131c55809945d06d
ExgleGaming <EaglesFan91x@outlook.com>
2026-01-29 22:32:46 -0500

    mining system

--- scripts/environment/rocks/rock.gd
+++ scripts/environment/rocks/rock.gd
@@ -12,3 +12,14 @@ var health: int
 func _ready() -> void:
     health = data.max_health
     sprite_2d.texture = data.texture
+
+
+func take_damage(amount: int) -> void:
+    health -= amount
+    print(health)
+    if health <= 0:
+        _destroy()
+
+
+func _destroy() -> void:
+    queue_free()
