a3358c7ec8669534083ea7dd20c42e12e364b19c
ExgleGaming <EaglesFan91x@outlook.com>
2026-01-29 21:44:10 -0500

    mining animations

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,17 +3,33 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
+var is_mining: bool = false
 var last_direction: Vector2 = Vector2.RIGHT
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
+func _ready() -> void:
+	animated_sprite_2d.animation_finished.connect(_on_animated_sprite_2d_animation_finished)
+
+
 func _physics_process(_delta: float) -> void:
+	# Handle mining input
+	if Input.is_action_just_pressed("use_pickaxe"):
+		use_pickaxe()
+	
+	# Skip movement if mining
+	if is_mining:
+		velocity= Vector2.ZERO
+		return
+
 	process_movement()	
 	process_animation()
 	move_and_slide()
 
-
+# - - - - - - - - - - - - - - - - - - - - - 
+# Movement and Animations
+# - - - - - - - - - - - - - - - - - - - - - 
 func process_movement() -> void:
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
@@ -39,3 +55,16 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 		animated_sprite_2d.play("%s_up" % prefix)
 	elif dir.y > 0:
 		animated_sprite_2d.play("%s_down" % prefix)
+
+
+# - - - - - - - - - - - - - - - - - - - - - 
+# Mining
+# - - - - - - - - - - - - - - - - - - - - - 
+func use_pickaxe() -> void:
+	is_mining = true
+	play_animation("swing_pickaxe", last_direction)
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if is_mining:
+		is_mining = false
