de14ab3804dacb6a2522cbdea3ab38da530e9c99
ExgleGaming <EaglesFan91x@outlook.com>
2026-01-31 19:54:26 -0500

    more rock types

--- scripts/levels/level.gd
+++ scripts/levels/level.gd
@@ -4,6 +4,10 @@ extends Node2D
 const FILL_PERCENTAGE: float = 0.4
 const ROCK_SCENE: PackedScene = preload("uid://c71x830xiqkhn")
 
+@export var rock_types: Array[RockData]
+
+var current_depth: int = 1
+
 @onready var rock_container: Node2D = $RockContainer
 @onready var current_map: Node2D = $Map
 @onready var player: Player = $Player
@@ -30,7 +34,7 @@ func _generate_rocks() -> void:
     var ground_cells: Array[Vector2i] = ground_layer.get_used_cells()
     var available_cells: Array = []
 
-    for cell in ground_cells:
+    for cell: Vector2i in ground_cells:
         # Get the tile data for this specific coordinate
         var tile_data: TileData = ground_layer.get_cell_tile_data(cell)
 
@@ -47,12 +51,35 @@ func _generate_rocks() -> void:
 
     var num_rocks: int = int(available_cells.size() * FILL_PERCENTAGE)
 
-    for i in range(num_rocks):
+    var valid_rocks: Array[RockData] = []
+    for rock: RockData in rock_types:
+        if current_depth >= rock.min_depth:
+            valid_rocks.append(rock)
+
+    for i: int in range(num_rocks):
         var cell = available_cells[i]
         var rock: Rock = ROCK_SCENE.instantiate()
+
+        rock.data = get_random_rock(valid_rocks)
 
         # Get local position from tilemap
         var local_position: Vector2 = ground_layer.map_to_local(cell)
 
         rock.global_position = local_position
         rock_container.add_child(rock)
+
+
+func get_random_rock(options: Array[RockData]) -> RockData:
+    var total_weight: int = 0
+    for rock: RockData in options:
+        total_weight += rock.rarity
+    
+    var roll: int = randi_range(0, total_weight - 1)
+    var current_sum: int = 0
+
+    for rock: RockData in options:
+        current_sum += rock.rarity
+        if roll < current_sum:
+            return rock
+    
+    return options[0] # Fallback to stone if nothing else is picked randomly
